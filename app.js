555e5830398c15e894d95ea2f48bd8cd7933fb11
JavaGirl18 <valenciacooper89@gmail.com>
2018-10-24 09:26:40 -0400

    commit message

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+require("dotenv").config();
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/idea-board
+
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');
+});
+// If the connection throws an error
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err);
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,8 +24,12 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(__dirname + '/client/build/'));
+
+  app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
