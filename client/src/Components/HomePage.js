8a38bc88dbe47c009b8f79d31b64ece58b0697e5
JavaGirl18 <valenciacooper89@gmail.com>
2018-12-19 13:09:42 -0500

    still troubleshooting heroku

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import GoogleLogin from 'react-google-login';
-// or
 import { GoogleLogin } from 'react-google-login';
 
 class HomePage extends Component {
